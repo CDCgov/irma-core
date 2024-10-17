93ab7313896bf59ff30f8e825dbe9ec49db8313f
dzw2 <dzw2@cdc.gov>
2024-10-17 16:22:47 -0400

    Left primer trimming using encoded kmers

--- src/processes/trimmer.rs
+++ src/processes/trimmer.rs
@@ -3,7 +3,9 @@
 use clap::Args;
 use either::Either;
 use std::{
+    collections::HashSet,
     fs::OpenOptions,
+    // hash::Hash,
     io::{stdin, BufReader},
     path::PathBuf,
 };
@@ -17,7 +19,7 @@ use crate::qc::fastq::*;
 pub struct TrimmerArgs {
     pub fastq_input_file: Option<PathBuf>,
 
-    #[arg(short = 'h', long)]
+    #[arg(short = 'H', long)]
     /// Hard trim from each end the specified length.
     pub hard_trim: Option<usize>,
 
@@ -36,6 +38,11 @@ pub struct TrimmerArgs {
     #[arg(short = 'U', long)]
     /// Re-encode FASTQ sequence to expected input: A, C, T, G, N
     pub canonical_bases: bool,
+
+    // primer trimming args to go here
+    #[arg(short = 'k', long)]
+    /// primers
+    pub fasta_primer_file: PathBuf,
 }
 
 //static MODULE: &str = module_path!();
@@ -50,33 +57,77 @@ pub fn trimmer_process(args: &TrimmerArgs) -> Result<(), std::io::Error> {
         FastQReader::new(BufReader::new(Either::Right(stdin())))
     };
 
-    let (forward_adapter, reverse_adapter) = match (&args.mask_adapter, &args.clip_adapter) {
+    let fasta_primer_reader = FastaReader::new(BufReader::new(OpenOptions::new().read(true).open(&args.fasta_primer_file)?));
+    let kmer_length = 17;
+    let restrict_left = 30;
+    let mut unique_kmers = HashSet::new();
+    let bases = [b'A', b'C', b'G', b'T'];
+
+    fasta_primer_reader.into_iter().for_each(|f| {
+        //let revc = reverse_complement(f);
+        let seq = f.unwrap().sequence;
+        if seq.len() > kmer_length {
+            for i in 0..=seq.len() - kmer_length {
+                let kmer = &seq[i..i + kmer_length];
+                unique_kmers.insert(kmer.to_vec());
+                unique_kmers.insert(reverse_complement(kmer));
+
+                for (pos, &original_base) in kmer.iter().enumerate() {
+                    for &base in &bases {
+                        if base != original_base {
+                            let mut variant = kmer.to_vec();
+                            variant[pos] = base;
+                            let revc = reverse_complement(&variant[..]);
+                            unique_kmers.insert(variant);
+                            unique_kmers.insert(revc);
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    /*let (forward_adapter, reverse_adapter) = match (&args.mask_adapter, &args.clip_adapter) {
         (Some(ref a), _) | (_, Some(ref a)) => {
             let forward = a.as_bytes().to_ascii_uppercase();
             let reverse = reverse_complement(&forward);
             (forward, reverse)
         }
         _ => (Vec::new(), Vec::new()),
-    };
+    };*/
 
     let mut i = 0;
+    let mut chopped = 0;
     for record in fastq_file_reader {
         let mut fq = record?;
 
-        fq.to_canonical_bases(args.canonical_bases)
-            .transform_by_reverse_forward_search(
-                args.fuzzy_adapter,
-                args.clip_adapter.is_some(),
-                &reverse_adapter,
-                &forward_adapter,
-            )
-            .hard_trim(args.hard_trim);
-
+        /*fq.to_canonical_bases(args.canonical_bases)
+                    .transform_by_reverse_forward_search(
+                        args.fuzzy_adapter,
+                        args.clip_adapter.is_some(),
+                        &reverse_adapter,
+                        &forward_adapter,
+                    )
+                    .hard_trim(args.hard_trim);
+        */
+        let max_index = fq.sequence[0..restrict_left]
+            .windows(kmer_length)
+            .enumerate()
+            .filter(|(_, k)| unique_kmers.contains(*k))
+            .max_by_key(|&(index, _)| index);
+        if let Some((max_index, _)) = max_index {
+            fq.hard_trim(Some(max_index + kmer_length));
+            chopped += 1;
+        }
         i += 1;
         print!("{fq}");
     }
 
+    // Primer trimming to go here
+
     eprintln!("Processed {i} reads.");
+    let percent = chopped as f64 / i as f64;
+    eprintln!("Chopped {} reads ({}%)", chopped, percent);
 
     Ok(())
 }
