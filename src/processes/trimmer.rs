9113a19bbe9cc694b2bcce2c65838d5bc53b8653
Sam Wiley <dzw2@cdc.gov>
2024-11-19 19:39:33 +0000

    Encoding kmers as u64 for hashing and searching

--- src/processes/trimmer.rs
+++ src/processes/trimmer.rs
@@ -1,5 +1,5 @@
 // Description:      Read FastQ files and trim with various options.
-
+#![allow(clippy::needless_range_loop)]
 use clap::Args;
 use either::Either;
 use foldhash::HashSet;
@@ -8,7 +8,9 @@ use std::{
     io::{stdin, BufReader, BufWriter, Write},
     path::PathBuf,
 };
-use zoe::{data::types::nucleotides::reverse_complement, prelude::*};
+use zoe::{data::to_dna_profile_index, data::types::nucleotides::reverse_complement, prelude::*};
+
+// use crate::qc::fastq::ReadTransforms;
 
 /* Assumed args */
 
@@ -42,19 +44,104 @@ pub struct TrimmerArgs {
     pub fasta_primer_file: PathBuf,
 }
 
+const BITS_PER_BASE: usize = 3;
+const MAX_KMER_SIZE: usize = 21;
+
+// pre-computing a table of set masks to quickly change a base within a pre-encoded kmer
+const SET_MASKS: [[u64; 5]; MAX_KMER_SIZE] = {
+    let mut set_masks = [[0u64; 5]; MAX_KMER_SIZE];
+    let mut index = 0;
+    while index < MAX_KMER_SIZE {
+        let mut base = 0;
+        while base < 5 {
+            set_masks[index][base] = (base << (3 * index)) as u64; // take the integer value of the base and move it over 3*i times to create a set mask for the given index, base pair
+            base += 1;
+        }
+        index += 1;
+    }
+    set_masks
+};
+
+// pre-computing a table of clear masks to quickly clear a base position within a pre-encoded kmer
+const CLEAR_MASKS: [u64; MAX_KMER_SIZE] = {
+    let mut clear_masks = [0u64; 21];
+    let mut index = 0;
+    while index < MAX_KMER_SIZE {
+        clear_masks[index] = !(0b111 << (3 * index)); // 111, we need to shift this 3*i times to get to the correct index, then negate it to put a 000, or empty base at that position
+        index += 1;
+    }
+    clear_masks
+};
+
+/// Encodes a kmer into a u64 with 3-bit representations
+fn encode_kmer(seq: &[u8]) -> u64 {
+    let mask = (1u64 << (BITS_PER_BASE * seq.len())) - 1;
+
+    let mut kmer: u64 = 0;
+
+    for &base in seq {
+        let base_val = to_dna_profile_index(base) as u64;
+        kmer = ((kmer << BITS_PER_BASE) | base_val) & mask;
+    }
+
+    kmer
+}
+
+/// Finds all possible 1 hamming distance variants of a given kmer and inserts them into a hash set
+/// Panics if primer.len() < kmer_length
+fn mutate_primer_add_kmers(primer: &[u8], kmer_length: usize, hash: &mut HashSet<u64>) {
+    let mut current_kmer = encode_kmer(&primer[primer.len() - kmer_length..]);
+    for i in (0..=primer.len() - kmer_length).rev() {
+        for index in 0..kmer_length {
+            for base in 0..b"ACGTN".len() {
+                let variant = (current_kmer & CLEAR_MASKS[index]) | SET_MASKS[index][base]; // clear the position of the changing base with &, then put the new base in with |
+                hash.insert(variant);
+            }
+        }
+        if i > 0 {
+            let next_base = primer[i - 1];
+            current_kmer = (current_kmer >> BITS_PER_BASE) | SET_MASKS[kmer_length - 1][to_dna_profile_index(next_base)];
+            // slide the window to include the next kmer
+        }
+    }
+}
+
+/// moves right to left within a range of a slice, checking if each encoded kmer is contained within the hash set
+/// if not, mutates the kmer using bitwise operations to slide the window
+fn find_kmer(sequence: &[u8], kmer_length: usize, unique_kmers: &HashSet<u64>) -> Option<usize> {
+    if sequence.len() < kmer_length {
+        return None;
+    }
+
+    let start_index = sequence.len() - kmer_length;
+    let mut current_kmer = encode_kmer(&sequence[start_index..]);
+
+    if unique_kmers.contains(&current_kmer) {
+        return Some(start_index);
+    }
+
+    //let mask = (1 << (3 * (kmer_length - 1))) - 1;
+    //not needed in a right-to-left implementation
+
+    for i in (0..start_index).rev() {
+        let next_base = sequence[i];
+        current_kmer = (current_kmer >> BITS_PER_BASE) | SET_MASKS[kmer_length - 1][to_dna_profile_index(next_base)];
+
+        if unique_kmers.contains(&current_kmer) {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
 //static MODULE: &str = module_path!();
 
 /// # Panics
 ///
 /// Sub-program for trimming fastQ data.
 pub fn trimmer_process(args: &TrimmerArgs) -> Result<(), std::io::Error> {
-    /*const MAP_N_TO_A: [u8; 256] = {
-        let mut out = [b'A'; 256];
-        out[b'C' as usize] = b'C';
-        out[b'G' as usize] = b'G';
-        out[b'T' as usize] = b'T';
-        out
-    };*/
+    let kmer_length = 17;
 
     let fastq_file_reader = if let Some(ref file_path) = args.fastq_input_file {
         FastQReader::new(BufReader::new(Either::Left(OpenOptions::new().read(true).open(file_path)?)))
@@ -64,30 +151,16 @@ pub fn trimmer_process(args: &TrimmerArgs) -> Result<(), std::io::Error> {
     let mut stdout_writer = BufWriter::new(std::io::stdout());
 
     let fasta_primer_reader = FastaReader::new(BufReader::new(OpenOptions::new().read(true).open(&args.fasta_primer_file)?));
-    let kmer_length = 17;
+
     let restrict_left = 30;
     let mut unique_kmers = HashSet::default();
 
     fasta_primer_reader.into_iter().for_each(|f| {
         let seq = f.unwrap().sequence;
         if seq.len() > kmer_length {
-            for i in 0..=seq.len() - kmer_length {
-                let kmer = &seq[i..i + kmer_length];
-                unique_kmers.insert(kmer.to_vec());
-                unique_kmers.insert(reverse_complement(kmer));
-
-                for (pos, &original_base) in kmer.iter().enumerate() {
-                    for &base in b"ACGTN" {
-                        if base != original_base {
-                            let mut variant = kmer.to_vec();
-                            variant[pos] = base;
-                            let revc = reverse_complement(&variant);
-                            unique_kmers.insert(variant);
-                            unique_kmers.insert(revc);
-                        }
-                    }
-                }
-            }
+            let rev_comp = reverse_complement(&seq);
+            mutate_primer_add_kmers(&seq, kmer_length, &mut unique_kmers);
+            mutate_primer_add_kmers(&rev_comp, kmer_length, &mut unique_kmers);
         }
     });
 
@@ -106,21 +179,16 @@ pub fn trimmer_process(args: &TrimmerArgs) -> Result<(), std::io::Error> {
         let mut fq = record?;
 
         /*fq.to_canonical_bases(args.canonical_bases)
-                    .transform_by_reverse_forward_search(
-                        args.fuzzy_adapter,
-                        args.clip_adapter.is_some(),
-                        &reverse_adapter,
-                        &forward_adapter,
-                    )
-                    .hard_trim(args.hard_trim);
+            .transform_by_reverse_forward_search(
+                args.fuzzy_adapter,
+                args.clip_adapter.is_some(),
+                &reverse_adapter,
+                &forward_adapter,
+            )
+            .hard_trim(args.hard_trim);
         */
-        let max_index = fq.sequence[0..restrict_left]
-            .windows(kmer_length)
-            .enumerate()
-            .rev()
-            .find(|(_, k)| unique_kmers.contains(*k))
-            .map(|(index, _)| index);
-        if let Some(max_index) = max_index {
+
+        if let Some(max_index) = find_kmer(&fq.sequence[0..restrict_left], kmer_length, &unique_kmers) {
             fq.sequence.cut_to_start(max_index + kmer_length);
             fq.quality.cut_to_start(max_index + kmer_length);
             chopped += 1;
@@ -128,8 +196,6 @@ pub fn trimmer_process(args: &TrimmerArgs) -> Result<(), std::io::Error> {
         i += 1;
         write!(stdout_writer, "{fq}")?;
     }
-
-    // Primer trimming to go here
 
     eprintln!("Processed {i} reads.");
     let percent = chopped as f64 / i as f64;
